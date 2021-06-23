1bb4abbe4bd8b05bb3d4dbfed7e570b064d4cb20
KICKdesigns <jwang@KICKdesigns.com>
2021-06-23 16:47:36 -0700

    SDK fixes

--- examples/sdktest.go
+++ examples/sdktest.go
@@ -3,7 +3,9 @@ package main
 import (
     "fmt"
     "github.com/paloaltonetworks/prisma-cloud-compute-go"
-    "github.com/paloaltonetworks/prisma-cloud-compute-go/collection"
+//    "github.com/paloaltonetworks/prisma-cloud-compute-go/collection"
+//    "github.com/paloaltonetworks/prisma-cloud-compute-go/policy/policyRuntimeContainer"
+    "github.com/paloaltonetworks/prisma-cloud-compute-go/policy/policyVulnerabilityImages"
 )
 
 func main() {
@@ -12,7 +14,8 @@ func main() {
     if err := client.Initialize("creds.json"); err != nil {
         fmt.Printf("Failed to connect: %s\n", err)
     }
-    
+
+/*    
     fmt.Printf("\nCREATE new Collection:\n")
     coll := collection.Collection{Name: "My Collection"}	
     err := collection.Create(client, coll)
@@ -69,4 +72,50 @@ func main() {
     for _, elm := range listing {
         fmt.Printf("* %s\n", elm.Name)
     }
+ */
+
+/*    fmt.Printf("\nGET Policy Runtime Container:\n")
+    singlePol, err := policyRuntimeContainer.Get(client)
+    if err != nil {
+        fmt.Printf("Failed to get single policy: %s\n", err)
+    }
+    fmt.Printf("* %s   %s\n", singlePol.PolicyId, singlePol.LearningDisabled)
+    
+    fmt.Printf("\nUPDATE new Policy:\n")
+    pol := policyRuntimeContainer.Policy{PolicyId: "My Policy", LearningDisabled: false}	
+    err = policyRuntimeContainer.Update(client, pol)
+    if err != nil {
+        fmt.Printf("Failed to get policies: %s\n", err)
+    }
+
+    fmt.Printf("\nGET Policy:\n")
+    singlePol, err = policyRuntimeContainer.Get(client)
+    if err != nil {
+        fmt.Printf("Failed to get single policy: %s\n", err)
+    }
+    fmt.Printf("* %s   %s\n", singlePol.PolicyId, singlePol.LearningDisabled)
+*/
+
+
+    fmt.Printf("\nGET Policy Vulnerability Images:\n")
+    singlePol, err := policyVulnerabilityImages.Get(client)
+    if err != nil {
+        fmt.Printf("Failed to get single policy: %s\n", err)
+    }
+    fmt.Printf("* %s   %s\n", singlePol.PolicyId, singlePol.PolicyType)
+    
+    fmt.Printf("\nUPDATE new Policy:\n")
+    pol := policyVulnerabilityImages.Policy{PolicyId: "My Policy", PolicyType: policyVulnerabilityImages.PolicyTypeContainerVulnerability}	
+    err = policyVulnerabilityImages.Update(client, pol)
+    if err != nil {
+        fmt.Printf("Failed to get policies: %s\n", err)
+    }
+
+    fmt.Printf("\nGET Policy:\n")
+    singlePol, err = policyVulnerabilityImages.Get(client)
+    if err != nil {
+        fmt.Printf("Failed to get single policy: %s\n", err)
+    }
+    fmt.Printf("* %s   %s\n", singlePol.PolicyId, singlePol.PolicyType)
+
 }
