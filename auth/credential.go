4b2fab852317d92b00cba127e9df0c5168345716
KICKdesigns <jwang@KICKdesigns.com>
2021-09-28 13:06:28 -0700

    Add credentials endpoint for SDK

--- auth/credential.go
+++ auth/credential.go
@@ -1,3 +1,68 @@
 package auth
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/paloaltonetworks/prisma-cloud-compute-go/pcc"
+)
+
 const CredentialsEndpoint = "api/v1/credentials"
+
+type Credential struct {
+	Id 		string           `json:"_id,omitempty"`
+	AccountGUID	string           `json:"accountGUID,omitempty"`
+	AccountID	string           `json:"accountID,omitempty"`
+	ApiToken	Secret           `json:"apiToken,omitempty"`
+	CaCert		string           `json:"caCert,omitempty"`
+	Created	string           `json:"created,omitempty"`
+	Description	string           `json:"description,omitempty"`
+	External	bool             `json:"external,omitempty"`
+	LastModified	string           `json:"lastModified,omitempty"`
+	Owner		string           `json:"owner,omitempty"`
+	RoleArn	string           `json:"roleArn,omitempty"`
+	Secret		Secret           `json:"secret,omitempty"`
+	SkipVerify	bool             `json:"skipVerify,omitempty"`
+	Tokens		[]TemporaryToken `json:"tokens,omitempty"`
+	Type		string         `json:"type,omitempty"`
+	Url		string           `json:"url,omitempty"`
+	UseAWSRole	bool             `json:"useAWSRole,omitempty"`
+}
+
+type Secret struct {
+	Encrypted    string           `json:"encrypted,omitempty"`
+	Plain        string           `json:"plain,omitempty"`
+}
+
+type TemporaryToken struct {
+	AwsAccessKeyId 	string           `json:"awsAccessKeyId,omitempty"`
+	AwsSecretAccessKey	Secret           `json:"awsSecretAccessKey,omitempty"`
+	Duration 		int              `json:"duration,omitempty"`
+	ExpirationTime 	string           `json:"expirationTime,omitempty"`
+	Token 			Secret           `json:"token,omitempty"`
+}
+
+
+// Get all credentials.
+func ListCredentials(c pcc.Client) ([]Credential, error) {
+	var ans []Credential
+	if err := c.Request(http.MethodGet, CredentialsEndpoint, nil, nil, &ans); err != nil {
+		return nil, fmt.Errorf("error listing credentials: %s", err)
+	}
+	return ans, nil
+}
+
+// Create a new credential.
+func CreateCredential(c pcc.Client, credential Credential) error {
+	return c.Request(http.MethodPost, CredentialsEndpoint, nil, credential, nil)
+}
+
+// Update an existing credential.
+func UpdateCredential(c pcc.Client, credential Credential) error {
+	return c.Request(http.MethodPut, CredentialsEndpoint, nil, credential, nil)
+}
+
+// Delete an existing credential.
+func DeleteCredential(c pcc.Client, name string) error {
+	return c.Request(http.MethodDelete, fmt.Sprintf("%s/%s", CredentialsEndpoint, name), nil, nil, nil)
+}
